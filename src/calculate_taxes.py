f6efb3c21c7319d6c8fe8c42dd2b544732006bfe
Elliott Bache <elliott.bache@gmail.com>
2025-11-19 13:16:03 +0100

    Added docstrings to draft of calculate_taxes.py.

--- src/calculate_taxes.py
+++ src/calculate_taxes.py
@@ -1,5 +1,38 @@
-# Utility to record a sale
-def record_sale(token, amount, proceeds, cost_basis, acquisition_date, sale_date):
+"""
+Calculate IRS capital gains taxes using FIFO method.
+
+Tax calculator that tracks capital gains from multiple purchases and sales.
+This program uses a CSV file as input.  This file is called "asset_tx.csv"
+in the published example, but any name can be used, using this name in
+the python call. The file has the following header: Date,Asset,Amount (asset),
+Sell Price ($),Buy Price ($),Account number,Entity,Notes,Remaining
+
+Functions:
+    record_sale:
+        Write a sale to the Form 8949 file object.
+
+    main:
+        Main function.
+
+"""
+
+def record_sale(asset, amount, proceeds, cost_basis, acquisition_date, sale_date):
+    """Record a sale.
+
+    This takes various data about the sale appends the data to the open Form 8949 file object.
+
+    Args:
+        asset (str): The asset name.
+        amount (str): The amount of the asset units.
+        proceeds (int): The dollar amount of capital gains.
+        cost_basis (str): The dollar cost of this amount of asset.
+        acquisition_date (str): The acquisition date.
+        sale_date (str): The sale date.
+
+    Returns:
+        int : 0 if no error.
+    """
+
     if proceeds >= 0.005 or cost_basis >= 0.005:
         form8949.append({
             "Description": f"{amount:.8f} {token}",
