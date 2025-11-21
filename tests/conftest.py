58e0292152bb609d55134022c17ba83b3452c8d8
Elliott Bache <elliott.bache@gmail.com>
2025-11-21 21:41:24 +0100

    Added lines to help tests find src folder in Windows PyCharm.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,13 @@
 import pytest
+import os
+import sys
+
+# Repo root = one level up from tests/
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+SRC = os.path.join(ROOT, "src")
+
+if SRC not in sys.path:
+    sys.path.insert(0, SRC)
 
 @pytest.fixture
 def readout(capsys) -> str:
